91cd734f5bb109dd7a167c0de1e036468f2a6dfa
anhhoang <anhhoang0319@gmail.com>
2024-03-23 14:33:00 -0700

    initialize backend

---
+++ App.js
@@ -0,0 +1,13 @@
+import express from "express";
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5.js";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+Hello(app);
+Lab5(app);
+app.listen(4000);
